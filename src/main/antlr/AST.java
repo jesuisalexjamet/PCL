47e1589442e8b32d4bb2f4ebe9c9905dfe3d0061
Alexandre V. Jamet <alexandrev.jamet@gmail.com>
2017-02-19 23:33:35 +0100

    Réécriture de fichier AST.java
    Modification d'un nom de méthode.
    
    Ajout d'une nouvelle méthode statique permettant la création d'un AST à partir d'un fichier source Looc.

--- src/main/antlr/AST.java
+++ src/main/antlr/AST.java
@@ -20,64 +20,88 @@ import main.antlr.LoocParser.program_return;
 import sun.reflect.generics.tree.Tree;
 
 public class AST {
-	private static ANTLRFileStream input;
+	private CommonTree tree;
+	
 	private static LoocLexer lexer;
 	private static CommonTokenStream tokens;
 	private static LoocParser parser;
-	private File out;
-	private FileWriter fileWriter;
-	private PrintWriter printWriter;
-	private List<String> tab=new ArrayList<String>();
+	private static DOTTreeGenerator dotTreeGenerator;
+	private static File out;
+	private static FileWriter fileWriter;
+	private static PrintWriter printWriter;
+	private static List<String> exampleList=new ArrayList<String>();
 	
-
-	private void setTab() {
-		tab.add("class");
-		tab.add("read");
-		tab.add("prio");
-		tab.add("if");
-		tab.add("class");
-		tab.add("fonction");
-		tab.add("for");
-		tab.add("full_example");
-		tab.add("inheritance");
+	public AST() {
+		this.tree = null;
 	}
 	
+	public AST(CommonTree tree) {
+		this.tree = tree;
+	}
 	
-	private void init(String filepath) throws Exception {
-		input = new ANTLRFileStream(filepath);
-		lexer = new LoocLexer(input);
+	public void setTree(CommonTree tree) {
+		this.tree = tree;
+	}
+	
+	public CommonTree getTree() {
+		return this.tree;
+	}
+	
+	public AST fromLoocFile(String filepath) throws Exception {
+		AST result = new AST();
+		
+		LoocLexer lexer = new LoocLexer(new ANTLRFileStream(filepath));
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		LoocParser parser = new LoocParser(tokens);
+		
+		result.setTree((CommonTree) parser.program().getTree());
+		
+		return result;
+	}
+
+	private static void fillExampleList() {
+		exampleList.add("class");
+		exampleList.add("read");
+		exampleList.add("prio");
+		exampleList.add("if");
+		exampleList.add("class");
+		exampleList.add("fonction");
+		exampleList.add("for");
+		exampleList.add("full_example");
+		exampleList.add("inheritance");
+	}
+	
+	private static void initializeTestingOutput(String filepath) throws Exception {
+		lexer = new LoocLexer(new ANTLRFileStream(filepath));
 		tokens = new CommonTokenStream(lexer);
 		parser = new LoocParser(tokens);
 	}
-	
-	private void getAST() throws Exception {
-		if(!new File("AST").exists()) {
-			new File("AST").mkdirs();
- 
-		}
-		for(int i=0;i<this.tab.size();i++){
-			this.init("tests/"+tab.get(i)+".looc");
-			CommonTree tree = (CommonTree)parser.program().getTree();
-			DOTTreeGenerator gen = new DOTTreeGenerator();
-			StringTemplate st=gen.toDOT(tree);
-			out=new File("AST/AST_"+tab.get(i)+".dot");
-			fileWriter = new FileWriter(out);
-			printWriter=new PrintWriter(fileWriter);
-			printWriter.print(st);
-			fileWriter.flush();
-			fileWriter.close();
-			System.out.println("Should be OK for "+"AST_"+tab.get(i));
-		}
-	}
 
-	
-	
-	
 	public static void main(String[] args) throws Exception {
-		AST ast=new AST();
-		ast.setTab();
-		ast.getAST();
-
+		AST.fillExampleList();
+		AST.dotTreeGenerator = new DOTTreeGenerator();
+		
+		AST exampleAST = new AST();
+		
+		if (!new File("AST").exists()) {
+			new File("AST").mkdirs();
+		}
+		
+		for (String example: AST.exampleList) {
+			AST.initializeTestingOutput("tests/" + example + ".looc");
+			exampleAST.setTree((CommonTree) AST.parser.program().getTree());
+			
+			StringTemplate st = AST.dotTreeGenerator.toDOT(exampleAST.getTree());
+			
+			AST.out = new File("AST/AST_" + example + ".dot");
+			AST.fileWriter = new FileWriter(AST.out);
+			AST.printWriter = new PrintWriter(AST.fileWriter);
+			AST.printWriter.print(st);;
+			AST.fileWriter.flush();
+			AST.fileWriter.close();
+			
+			System.out.println("Should be OK for AST_" + example);
+		}
 	}
 
 }
