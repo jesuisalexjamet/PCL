718bb8f5de404858442ade8c7d780a5fda4cfc8f
hell9u <nicolas.hell@telecomnancy.eu>
2017-03-29 15:51:20 +0200

    First semantic control on arithmetic operations

--- src/main/symbols/controls/CheckArithmetique.java
+++ src/main/symbols/controls/CheckArithmetique.java
@@ -1,5 +1,32 @@
 package main.symbols.controls;
 
-public abstract class CheckArithmetique {
+import main.antlr.errors.AbstractSemanticErrorReporter;
+import main.symbols.Symbol;
+import main.symbols.SymbolTable;
 
+//Made by N.Hell
+public abstract class CheckArithmetique {
+	
+	public static void checkOperation(String left, String right, SymbolTable ST, AbstractSemanticErrorReporter reporter) {
+		if (!left.matches("-?[0:9]+")) {
+			Symbol leftSymbol = ST.getSymbol(left);
+			if (leftSymbol == null) {
+				reporter.reportError("Left operand doesn't exist");
+			}
+			else if (!(leftSymbol.getType().getName().equals("int"))) {
+				reporter.reportError("Left operand is not an int");
+			}
+		}
+		if (!right.matches("-?[0:9]+")) {
+			Symbol rightSymbol = ST.getSymbol(right);
+			if (rightSymbol == null) {
+				reporter.reportError("Right operand doesn't exist");
+			}
+			else if (!(rightSymbol.getType().getName().equals("int"))) {
+				reporter.reportError("Right operand is not an int");
+			}
+		}	
+	}
+	
 }
+
