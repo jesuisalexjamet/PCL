3725bb30b5512255567019eeb3da1fafe274d730
chone12u <antoine.chone@telecomnancy.net>
2017-03-29 22:11:20 +0200

    Ajout de test + correctifs mineurs

--- src/main/symbols/controls/CheckComparaison.java
+++ src/main/symbols/controls/CheckComparaison.java
@@ -7,7 +7,8 @@ import main.symbols.SymbolTable;
 
 public abstract class CheckComparaison {
 	public static void checkComparaison(String left, String right, SymbolTable ST, AbstractSemanticErrorReporter reporter) {
-		if (!left.matches("-?[0:9]")) {
+
+		if (!left.matches("-?[0:9]+")) {
 			Symbol leftSymbol = ST.getSymbol(left);
 			if (leftSymbol == null) {
 				reporter.reportError("Left operand doesn't exist");
@@ -17,12 +18,12 @@ public abstract class CheckComparaison {
 			}
 			
 		}
-		if (right.matches("-?[0:9]+")) {
+		if (!right.matches("-?[0:9]+")) {
 			Symbol rightSymbol = ST.getSymbol(right);
 			if (rightSymbol == null) {
 				reporter.reportError("Right operand doesn't exist");
 			}
-			else if (rightSymbol.getType().getName().equals("int")) {
+			else if (!rightSymbol.getType().getName().equals("int")) {
 				reporter.reportError("Right operand is not an int or boolean");
 			}
 		}
