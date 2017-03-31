9a1740e92fe8d648d683c4f37aec4807977eaa5c
NicolasHell <tiphchtn@MacBook-Pro-de-Tiph.local>
2017-03-31 20:33:43 +0200

    Contrôle sur l'affectation (incomplet pour le moment)

--- src/main/symbols/controls/CheckAffectation.java
+++ src/main/symbols/controls/CheckAffectation.java
@@ -1,5 +1,27 @@
 package main.symbols.controls;
 
+import main.antlr.errors.AbstractSemanticErrorReporter;
+import main.symbols.Symbol;
+import main.symbols.SymbolTable;
+
 public abstract class CheckAffectation {
+	
+	public static void checkAffectation(String left, String right, SymbolTable ST, AbstractSemanticErrorReporter reporter) {
+		
+		Symbol leftSymbol = ST.getSymbol(left);
+		CheckDeclaration.checkVariableExistence(left,ST,reporter);
+		if (right.matches("-?[0-9]+")) {
+			if (!(leftSymbol.getType().getName().equals("int"))) {
+				reporter.reportError(left + " is not an int and is affected to an int");
+			}
+		}
+		else if (!(right.matches("-?[+,-,*,/,>,<,<=,>=,%]"))) {
+			Symbol rightSymbol = ST.getSymbol(right);
+			if (!(leftSymbol.getType().getName().equals(rightSymbol.getType().getName()))) {
+				reporter.reportError(left + " and " + right + " are not the same type");
+			}
+		}
+		
+	}
 
 }
