2de0b50d2d081eae5957644df8cb5944bf0bdf41
RAOUX Killian <killian.raux@gmail.com>
2017-04-04 11:45:19 +0200

    apres le speudo apocalypse

--- src/main/symbols/ClassSymbol.java
+++ src/main/symbols/ClassSymbol.java
@@ -3,15 +3,42 @@ package main.symbols;
 public class ClassSymbol extends Symbol {
 	private SymbolTable childSymbolTable;
 	private ClassSymbol parentClass;
+	/**
+	 * Constructeur avec heritage
+	 * @param name
+	 * @param parentSymbolTable
+	 * @param parentClass
+	 */
 	public ClassSymbol(String name, SymbolTable parentSymbolTable,String parentClass) {
 		super(name, "class", parentSymbolTable);
 		this.parentClass = (ClassSymbol) parentSymbolTable.getSymbol(parentClass);
 		this.childSymbolTable = new SymbolTable(this.parentSymbolTable,name);
 	}
+	/**
+	 * Constructeur sans heritage
+	 * @param name
+	 * @param parentSymbolTable
+	 */
 	public ClassSymbol(String name, SymbolTable parentSymbolTable) {
 		super(name, "class", parentSymbolTable);
 		this.parentClass = null;
 		this.childSymbolTable = new SymbolTable(this.parentSymbolTable,name);
+	}
+	/**
+	 * getMethod donne la method associé au nom. Cherche d'abord dans la TDS de la class puis dans celle de la class herité.
+	 * @param name nom de la method
+	 * @return la method
+	 */
+	public Method getMethod(String name){
+		for (Symbol s: this.childSymbolTable){
+			if ((s instanceof Method) && (s.getName().equals(name))){
+				return (Method) s;
+			}
+		}
+		if (this.parentClass != null){
+			return this.parentClass.getMethod(name);
+		}
+		return null;
 	}
 	@Override
 	public SymbolTable getChildSymbolTable() {
