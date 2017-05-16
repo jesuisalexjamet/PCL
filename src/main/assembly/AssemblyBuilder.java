0fe48060a659be159d65ee170221453abbcfd6ed
hell9u <nicolas.hell@telecomnancy.eu>
2017-05-16 11:51:49 +0200

    Assembleur pour les opérations (incomplet car exception division par 0 non gérée et opérations utilisant des variables non gérées

--- src/main/assembly/AssemblyBuilder.java
+++ src/main/assembly/AssemblyBuilder.java
@@ -1,7 +1,12 @@
 package main.assembly;
 
+import java.util.List;
+
+import org.antlr.runtime.tree.CommonTree;
+
 import main.compiler.Program;
 import main.symbols.Symbol;
+import main.symbols.SymbolTable;
 import main.symbols.Variable;
 
 /**
@@ -54,8 +59,8 @@ public class AssemblyBuilder {
 	 */
 	public String translateProgram(Program prg) {
 		String assPrg = this.bootstrap(prg);
-		assPrg += this.translateGlobalVariables(prg) + "\n\n";
-		
+		//assPrg += this.translateGlobalVariables(prg) + "\n\n";
+		assPrg += this.translateArithmetic(prg.getAbstractTree(), prg.getSymbolTable());
 		return assPrg;
 	}
 	
@@ -115,6 +120,69 @@ public class AssemblyBuilder {
 		return res;
 	}
 	
+	/**
+	 * Méthode permettant d'obtenir une représentation sous forme de langage d'assemblage les affectations de variables
+	 * 
+	 * @param tree, ST
+	 * @return
+	 */
+	private String translateAffectation(CommonTree tree, SymbolTable ST) {
+		String res = "";
+		String childOne = tree.getChild(0).getText();
+		String childTwo = tree.getChild(1).getText();
+	
+		return res;
+	}
+	
+	/**
+	 * Méthode permettant d'obtenir une représentation sous forme de langage d'assemblage les opérations arithmétiques
+	 * 
+	 * @param tree, ST
+	 * @return
+	 */
+	private String translateArithmetic(CommonTree tree, SymbolTable ST) {
+		String res = "";
+		if (tree==null) {
+			return "";
+		}
+		String operation = tree.getText();
+		if (!operation.matches("([+,1,*,/])")){
+			res += translateArithmetic((CommonTree) tree.getChild(0), ST);
+			res += translateArithmetic((CommonTree) tree.getChild(1), ST);
+			return res;
+		}
+		CommonTree childOne = (CommonTree) tree.getChild(0);
+		CommonTree childTwo = (CommonTree) tree.getChild(1);
+		if (childOne.getText().matches("([+,-,*,/])")) {
+			res += translateArithmetic(childOne,ST);
+			res += "STW R3 R1\n";
+		}
+		else if (childOne.getText().matches("[0-9]+")) {
+			res = res + "STW R1 "+childOne.getText() + "\n";
+		}
+		else {
+			
+			res+="coucou";
+		}
+		if (childTwo.getText().matches("([+,-,*,/])")) {
+			res += translateArithmetic(childTwo,ST);
+			res += "STW R3 R2\n";
+		}
+		else if (childTwo.getText().matches("[0-9]+")) {
+			res = res + "STW R2 "+childTwo.getText() + "\n";
+		}
+		else {
+			
+		}
+		
+		switch (operation) {
+		case "+": res += "ADD R1 R2 R3 \n"; break;
+		case "-": res += "SUB R1 R2 R3 \n"; break;
+		case "*": res += "MUL R1 R2 R3 \n"; break;
+		case "/": res += "DIV R1 R2 R3 \n"; break;
+	}
+		return res;
+	}
 	/**
 	 * Méthode permettant d'obtenir une représentation sous forme de langage d'assemblage
 	 * des instructions présentent dans le portée globale.
